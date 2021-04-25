5a9b1da7640054c16ad576430ed116040107bf22
Kevin Kruger <kruger813@gmail.com>
2021-04-25 18:57:21 -0400

    Added Checkout class and implemented exceptions.

---
+++ src/Checkout.java
@@ -0,0 +1,65 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Checkout {
+
+    // Checkout values
+    private String toolCode;
+    private int rentalDayCount;
+    private int discountPercent;
+    private String checkOutDate;
+    private Date validatedDate;
+
+    // Checkout constructor
+    public Checkout(String toolCode, int rentalDayCount, int discountPercent, String checkOutDate){
+        this.toolCode = toolCode;
+        this.rentalDayCount = rentalDayCount;
+        this.discountPercent = discountPercent;
+        this.checkOutDate = checkOutDate;
+        validate();
+    }
+
+    // Method used to validate checkout inputs
+    private void validate(){
+        switch (toolCode){
+            case "LADW":
+            case "CHNS":
+            case "JAKR":
+            case "JAKD":
+                break;
+            default:
+                throw new InvalidInputException("Please input a valid tool code.");
+        }
+        if(rentalDayCount < 1){
+            throw new InvalidInputException("Rental day count must be 1 or greater.");
+        }
+        if(discountPercent < 0 || discountPercent > 100){
+            throw new InvalidInputException("Discount Percent must be between 0 and 100.");
+        }
+        try{
+            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
+            validatedDate = dateFormat.parse(checkOutDate);
+        }
+        catch (ParseException e){
+            throw new InvalidInputException("Please enter a valid date formatted as mm/dd/yy.");
+        }
+    }
+
+    // Method used to generate Rental Agreement from current Checkout
+    public RentalAgreement generateRentalAgreement() throws ParseException {
+        switch (toolCode){
+            case "LADW":
+               return new LadderRentalAgreement(ToolCode.LADW, rentalDayCount, validatedDate, discountPercent);
+            case "CHNS":
+                return new ChainsawRentalAgreement(ToolCode.CHNS, rentalDayCount, validatedDate, discountPercent);
+            case "JAKR":
+                return new JackhammerRentalAgreement(ToolCode.JAKR, rentalDayCount, validatedDate, discountPercent);
+            case "JAKD":
+                return new JackhammerRentalAgreement(ToolCode.JAKD, rentalDayCount, validatedDate, discountPercent);
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/InvalidInputException.java
@@ -0,0 +1,5 @@
+public class InvalidInputException extends RuntimeException{
+    public InvalidInputException(String errorMessage) {
+        super(errorMessage);
+    }
+}
